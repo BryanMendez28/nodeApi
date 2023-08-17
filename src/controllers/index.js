a60de00ccb56f028c929af22d11dfcd6bdc77944
Bryan <alanm.ceron@gmail.com>
2023-08-17 15:33:34 -0600

    Nueva Consulta

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -50,3 +50,174 @@ exports.getTabla = async (req, res) => {
     });
   };
   
+
+
+exports.getTotal = async (req, res) => {
+    // Intenta imprimir los parámetros que estás recibiendo para asegurarte de que son correctos
+    console.log(req.query);
+  
+    const {
+      fechaInicio,
+      fechaFin, 
+      horaInicio,
+      horaFin,
+      nombreMaquinaFiltro,
+      codeProductFiltro
+    } = req.query;
+  
+    req.getConnection((err, conn) => {
+      if (err) {
+        console.error('Error connecting to database:', err);
+        return res.status(500).json({ error: 'Internal Server Error' });
+      }
+  
+      const queryTotalGastadoTarjetaCredito = `
+        SELECT SUM(A.SeValue) AS TotalGastadoTarjetaCredito
+        FROM nayax_transacciones A
+        JOIN nayax_temp B ON B.id = A.cliente_id
+        WHERE A.MachineSeTimeDateOnly BETWEEN ? AND ?
+          AND A.MachineSeTimeTimeOnly BETWEEN ? AND ?
+          AND B.nombre LIKE ?
+          AND A.ProductCodeInMap LIKE ?
+          AND A.PaymentMethodId = 1
+      `;
+  
+      const queryTotalGastadoEfectivo = `
+        SELECT SUM(A.SeValue) AS TotalGastadoEfectivo
+        FROM nayax_transacciones A
+        JOIN nayax_temp B ON B.id = A.cliente_id
+        WHERE A.MachineSeTimeDateOnly BETWEEN ? AND ?
+          AND A.MachineSeTimeTimeOnly BETWEEN ? AND ?
+          AND B.nombre LIKE ?
+          AND A.ProductCodeInMap LIKE ?
+          AND A.PaymentMethodId = 3
+      `;
+  
+      const queryTotalPiezasVendidas = `
+        SELECT COUNT(*) AS TotalPiezasVendidas
+        FROM nayax_transacciones A
+        JOIN nayax_temp B ON B.id = A.cliente_id
+        WHERE A.MachineSeTimeDateOnly BETWEEN ? AND ?
+          AND A.MachineSeTimeTimeOnly BETWEEN ? AND ?
+          AND B.nombre LIKE ?
+          AND A.ProductCodeInMap LIKE ?
+      `;
+  
+      const queryTarjetaCredito = `
+        SELECT COUNT(*) AS TotalTarjetaCredito
+        FROM nayax_transacciones A
+        JOIN nayax_temp B ON B.id = A.cliente_id
+        WHERE A.MachineSeTimeDateOnly BETWEEN ? AND ?
+          AND A.MachineSeTimeTimeOnly BETWEEN ? AND ?
+          AND B.nombre LIKE ?
+          AND A.ProductCodeInMap LIKE ?
+          AND PaymentMethodId = 1
+      `;
+  
+      const queryEfectivo = `
+        SELECT COUNT(*) AS TotalEfectivo
+        FROM nayax_transacciones A
+        JOIN nayax_temp B ON B.id = A.cliente_id
+        WHERE A.MachineSeTimeDateOnly BETWEEN ? AND ?
+          AND A.MachineSeTimeTimeOnly BETWEEN ? AND ?
+          AND B.nombre LIKE ?
+          AND A.ProductCodeInMap LIKE ?
+          AND PaymentMethodId = 3
+      `;
+  
+      const queryTotalGastado = `
+        SELECT SUM(A.SeValue) AS TotalGastado
+        FROM nayax_transacciones A
+        JOIN nayax_temp B ON B.id = A.cliente_id
+        WHERE A.MachineSeTimeDateOnly BETWEEN ? AND ?
+          AND A.MachineSeTimeTimeOnly BETWEEN ? AND ?
+          AND B.nombre LIKE ?
+          AND A.ProductCodeInMap LIKE ?
+      `;
+  
+      conn.query(
+        queryTotalGastadoTarjetaCredito,
+        [fechaInicio, fechaFin, horaInicio, horaFin, `%${nombreMaquinaFiltro}%`, `%${codeProductFiltro}%`],
+        (errTotalGastadoTarjetaCredito, rowsTotalGastadoTarjetaCredito) => {
+          if (errTotalGastadoTarjetaCredito) {
+            console.error('Error executing query Total Gastado Tarjeta Crédito:', errTotalGastadoTarjetaCredito);
+            return res.status(500).json({ error: 'Internal Server Error' });
+          }
+  
+          conn.query(
+            queryTotalGastadoEfectivo,
+            [fechaInicio, fechaFin, horaInicio, horaFin, `%${nombreMaquinaFiltro}%`, `%${codeProductFiltro}%`],
+            (errTotalGastadoEfectivo, rowsTotalGastadoEfectivo) => {
+              if (errTotalGastadoEfectivo) {
+                console.error('Error executing query Total Gastado Efectivo:', errTotalGastadoEfectivo);
+                return res.status(500).json({ error: 'Internal Server Error' });
+              }
+  
+              conn.query(
+                queryTotalPiezasVendidas,
+                [fechaInicio, fechaFin, horaInicio, horaFin, `%${nombreMaquinaFiltro}%`, `%${codeProductFiltro}%`],
+                (errTotalPiezasVendidas, rowsTotalPiezasVendidas) => {
+                  if (errTotalPiezasVendidas) {
+                    console.error('Error executing query Total Piezas Vendidas:', errTotalPiezasVendidas);
+                    return res.status(500).json({ error: 'Internal Server Error' });
+                  }
+  
+                  conn.query(
+                    queryTarjetaCredito,
+                    [fechaInicio, fechaFin, horaInicio, horaFin, `%${nombreMaquinaFiltro}%`, `%${codeProductFiltro}%`],
+                    (errTarjetaCredito, rowsTarjetaCredito) => {
+                      if (errTarjetaCredito) {
+                        console.error('Error executing query Tarjeta Crédito:', errTarjetaCredito);
+                        return res.status(500).json({ error: 'Internal Server Error' });
+                      }
+  
+                      conn.query(
+                        queryEfectivo,
+                        [fechaInicio, fechaFin, horaInicio, horaFin, `%${nombreMaquinaFiltro}%`, `%${codeProductFiltro}%`],
+                        (errEfectivo, rowsEfectivo) => {
+                          if (errEfectivo) {
+                            console.error('Error executing query Efectivo:', errEfectivo);
+                            return res.status(500).json({ error: 'Internal Server Error' });
+                          }
+  
+                          conn.query(
+                            queryTotalGastado,
+                            [fechaInicio, fechaFin, horaInicio, horaFin, `%${nombreMaquinaFiltro}%`, `%${codeProductFiltro}%`],
+                            (errTotalGastado, rowsTotalGastado) => {
+                              if (errTotalGastado) {
+                                console.error('Error executing query Total Gastado:', errTotalGastado);
+                                return res.status(500).json({ error: 'Internal Server Error' });
+                              }
+  
+                              // Procesa los resultados y crea el objeto de respuesta final
+                              const totalGastadoTarjetaCredito = rowsTotalGastadoTarjetaCredito[0].TotalGastadoTarjetaCredito;
+                              const totalGastadoEfectivo = rowsTotalGastadoEfectivo[0].TotalGastadoEfectivo;
+                              const totalPiezasVendidas = rowsTotalPiezasVendidas[0].TotalPiezasVendidas;
+                              const totalTarjetaCredito = rowsTarjetaCredito[0].TotalTarjetaCredito;
+                              const totalEfectivo = rowsEfectivo[0].TotalEfectivo;
+                              const totalGastado = rowsTotalGastado[0].TotalGastado;
+  
+                              const resultadoFinal = {
+                                TotalTarjetaCredito: totalTarjetaCredito,
+                                TotalEfectivo: totalEfectivo,
+                                TotalPiezasVendidas: totalPiezasVendidas,
+                                TotalGastado: totalGastado,
+                                TotalGastadoTarjetaCredito: totalGastadoTarjetaCredito,
+                                TotalGastadoEfectivo: totalGastadoEfectivo
+                              };
+  
+                              res.json(resultadoFinal);
+                            }
+                          );
+                        }
+                      );
+                    }
+                  );
+                }
+              );
+            }
+          );
+        }
+      );
+    });
+  };
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const route = express.Router();
-const {get, getTabla} = require("../controllers/index")
+const {get, getTabla, getTotal} = require("../controllers/index")
 
 
 route.get("/prueba", get);
 route.get("/consulta", getTabla);
+route.get("/", getTotal);
 
 module.exports = route;
