66fc1859972275179369d1a2bf2de3c82199b891
Bryan <alanm.ceron@gmail.com>
2023-08-17 17:20:29 -0600

    getTotal

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -52,6 +52,52 @@ exports.getTabla = async (req, res) => {
   
 
 
+exports.getTablaTotal = async (req, res) => {
+  let {
+    fechaInicio ,
+    fechaFin ,
+    horaInicio ,
+    horaFin ,
+    cliente_id ,
+  } = req.query;
+
+  req.getConnection((err, conn) => {
+    if (err) return res.send(err);
+
+    const query = `
+   
+  SELECT
+  SUM(TotalRegistros) AS SumaTotalRegistros,
+  SUM(TotalRegistrosTarjetaCredito) AS SumaTotalRegistrosTarjetaCredito,
+  SUM(TotalRegistrosEfectivo) AS SumaTotalRegistrosEfectivo,
+  SUM(TotalGastadoTarjetaCredito) AS SumaTotalGastadoTarjetaCredito,
+  SUM(TotalGastadoEfectivo) AS SumaTotalGastadoEfectivo,
+  SUM(TotalCosto) AS SumaTotalCosto
+FROM (
+  SELECT 
+    A.ProductCodeInMap,
+    COUNT(*) AS TotalRegistros,
+    SUM(CASE WHEN A.PaymentMethodId = 1 THEN 1 ELSE 0 END) AS TotalRegistrosTarjetaCredito,
+    SUM(CASE WHEN A.PaymentMethodId = 3 THEN 1 ELSE 0 END) AS TotalRegistrosEfectivo,
+    SUM(CASE WHEN A.PaymentMethodId = 1 THEN SeValue ELSE 0 END) AS TotalGastadoTarjetaCredito,
+    SUM(CASE WHEN A.PaymentMethodId = 3 THEN SeValue ELSE 0 END) AS TotalGastadoEfectivo,
+    SUM(SeValue) AS TotalCosto
+  FROM nayax_transacciones A 
+  JOIN nayax_temp B ON B.id= A.cliente_id
+  WHERE A.MachineSeTimeDateOnly BETWEEN ? AND ?
+    AND A.MachineSeTimeTimeOnly BETWEEN ? AND ? 
+    AND B.nombre LIKE ? 
+  GROUP BY A.ProductCodeInMap
+) AS Subconsulta;
+    `;
+
+    conn.query(query, [fechaInicio, fechaFin, horaInicio, horaFin, `${cliente_id}%`], (err, result) => {
+      if (err) return res.send(err);
+      res.send(result);
+    });
+  });
+};
+
 exports.getTotal = async (req, res) => {
     // Intenta imprimir los parámetros que estás recibiendo para asegurarte de que son correctos
     console.log(req.query);
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const route = express.Router();
-const {get, getTabla, getTotal} = require("../controllers/index")
+const {get, getTabla, getTotal, getTablaTotal} = require("../controllers/index")
 
 
 route.get("/prueba", get);
 route.get("/consulta", getTabla);
 route.get("/", getTotal);
+route.get("/total", getTablaTotal);
 
 module.exports = route;
